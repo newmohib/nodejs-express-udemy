f3048a627d9cb45a0735b0af74fad5eafd5719ba
unknown <newmohib@gmail.com>
2019-11-29 20:21:29 +0600

    hapi/joi added

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
-const app = express()
+const Joi = require('@hapi/joi');
 
-
+const app = express();
+app.use(express.json());
 
 var courses = [
     { id: 1, name: "course 1" },
@@ -19,8 +20,8 @@ app.get('/api/courses', (req, res) => {
 
 app.get('/api/courses/:id', (req, res) => {
     let id = req.params.id;
-    const course=courses.find(item=>item.id === parseInt(id))
-    if(!course){
+    const course = courses.find(item => item.id === parseInt(id))
+    if (!course) {
         res.status(404).send('The Course With given ID id not foundes')
     }
     res.send(course);
@@ -36,6 +37,26 @@ app.get('/api/query/:year/:month', (req, res) => {
     res.send(query);
 });
 
+
+app.post('/api/courses', (req, res) => {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required(),
+        username:Joi.string().required(),
+    })
+    const { error, value } = schema.validate(req.body,{ abortEarly: false });
+
+    if (error !== undefined) {
+        res.status(400).send('Name is Required and should be minimum 3 characters.');
+    }
+    console.log("value", value);
+    console.log("error", error);
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    }
+    courses.push(course);
+    res.send(course);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
